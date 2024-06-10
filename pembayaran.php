e8749eef7b467480511ee6faa7319ef61a716235
Enggal0 <enggalbimas8@gmail.com>
2024-06-10 22:57:47 +0700

    ui informasi pembayaran

--- pembayaran.php
+++ pembayaran.php
@@ -10,6 +10,7 @@ include 'database/auth.php';
     <title>Kliket - Pembayaran</title>
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/pembayaran.css">
+    <link rel="stylesheet" href="css/font.css">
 </head>
 <body>
     <!-- Navbar -->
@@ -19,7 +20,8 @@ include 'database/auth.php';
 
     <!-- Pembayaran -->
     <div class="container">
-        <h1>Informasi Pembayaran</h1>
+        <h1 style='color: #5c5858'>Informasi Pembayaran</h1>
+        <hr>
         <?php
         if (isset($_GET['tiketID'])) {
             $tiketID = $_GET['tiketID'];
@@ -33,18 +35,18 @@ include 'database/auth.php';
             $result = $stmt->get_result();
             if ($result->num_rows > 0) {
                 $row = $result->fetch_assoc();
-                ?>
-                <p>Asal: <?php echo htmlspecialchars($row['asal']); ?></p>
-                <p>Tujuan: <?php echo htmlspecialchars($row['tujuan']); ?></p>
-                <p>Tanggal Berangkat: <?php echo htmlspecialchars($row['tanggal_berangkat']); ?></p>
-                <p>Waktu Berangkat: <?php echo htmlspecialchars($row['waktu_berangkat']); ?></p>
-                <p>Harga: Rp <?php echo htmlspecialchars($row['harga']); ?></p>
-                <p>Kode Unik Bank Virtual: <?php echo htmlspecialchars($row['kode_unik_bank']); ?></p>
+                ?> 
+                <p style='color: #5c5858; padding: 10px'>Asal: <?php echo htmlspecialchars($row['asal']); ?></p>
+                <p style='color: #5c5858; padding: 10px'>Tujuan: <?php echo htmlspecialchars($row['tujuan']); ?></p>
+                <p style='color: #5c5858; padding: 10px'>Tanggal Berangkat: <?php echo htmlspecialchars($row['tanggal_berangkat']); ?></p>
+                <p style='color: #5c5858; padding: 10px'>Waktu Berangkat: <?php echo htmlspecialchars($row['waktu_berangkat']); ?></p>
+                <p style='color: #5c5858; padding: 10px'>Harga: Rp<?php echo htmlspecialchars($row['harga']); ?></p>
+                <p style='color: #5c5858; padding: 10px'>Kode Unik Bank Virtual: <?php echo htmlspecialchars($row['kode_unik_bank']); ?></p>
                 
                 <!-- Form untuk upload bukti pembayaran -->
                 <form action="database/checkout.php" method="POST" enctype="multipart/form-data">
                     <input type="hidden" name="tiketID" value="<?php echo htmlspecialchars($tiketID); ?>">
-                    <label for="bukti_pembayaran">Upload Bukti Pembayaran:</label>
+                    <label for="bukti_pembayaran" style='color: #5c5858; padding: 10px'>Upload Bukti Pembayaran:</label>
                     <input type="file" name="bukti_pembayaran" id="bukti_pembayaran" accept="bukti/*" required>
                     <button type="submit" class="btn-pembayaran">Bayar</button>
                 </form>
