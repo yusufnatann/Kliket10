70c40120798c2acdad74d5c92fa2debb868160d0
atnatr <atrizki.anta@gmail.com>
2024-06-10 19:07:31 +0700

    Update : - Ganti desain detail

--- navbar.php
+++ navbar.php
@@ -19,12 +19,6 @@
                     <li><a href="pesanan.php"><i class="fa-solid fa-receipt icon"></i>Pesanan Saya</a></li>
                     <li><a href="riwayat.php"><i class="fa-solid fa-clock-rotate-left icon"></i>Riwayat Tiket</a></li>
                     <li><a href="faq.php"><img src="img/faq.png" class="icon">FAQ</a></li>
-                    <?php if (($_SESSION['kategoriID']) === 1): ?>
-                    <li><a href="admin/admutama.php"><img src="" class="icon">Dashboard Admin</a></li>
-                    <?php endif; ?>
-                    <?php if (($_SESSION['kategoriID']) === 1 || ($_SESSION['kategoriID']) === 2): ?>
-                    <li><a href="indexPetugas.php"><img src="" class="icon">Petugas</a></li>
-                    <?php endif; ?>
                     <li><a href="database/logout.php"><img src="img/keluar.png" class="icon">Keluar</a></li>
                 </ul>
             </div>
